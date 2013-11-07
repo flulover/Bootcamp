00fdf3bb9f5b9240eca2206b1065d1c7d07ae44f
flulover <flulover@126.com>
2013-11-07 23:23:25 +0800

    Add robot to manage locker.

---
+++ SuperMarket/Robot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SuperMarket
+{
+    public class Robot
+    {
+        private readonly IList<Locker> _lockerList;
+
+        public Robot(IList<Locker> lockerList)
+        {
+            _lockerList = lockerList;
+        }
+
+        public Ticket Store(Bag bag)
+        {
+            return _lockerList.Select(locker => locker.Store(bag)).FirstOrDefault(ticket => ticket != null);
+        }
+
+        public Bag Pick(Ticket ticket)
+        {
+            foreach (var locker in _lockerList)
+            {
+                var bag = locker.Pick(ticket);
+                if (bag != null)
+                {
+                    return bag;
+                }
+            }
+            return null;
+        }
+    }
+}
--- SuperMarketTest/SuperMarketTest.cs
+++ SuperMarketTest/SuperMarketTest.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading;
 using NUnit.Framework;
 using SuperMarket;
@@ -57,6 +58,64 @@ namespace SuperMarketTest
 
             locker.Pick(ticket);
             Assert.Null(locker.Pick(ticket));
+        }
+
+        [Test]
+        public void should_return_a_valid_ticket_when_locker_availiable()
+        {
+            var lockers = new List<Locker>();
+            var locker = new Locker(1);
+            lockers.Add(locker);
+
+            var robot = new Robot(lockers);
+            var bag = new Bag();
+            var ticket = robot.Store(bag);
+
+            Assert.AreSame(locker.Pick(ticket), bag);
+        }
+
+        [Test]
+        public void should_return_null_when_no_locker_availiable()
+        {
+            var lockers = new List<Locker> {new Locker(1)};
+
+            var robot = new Robot(lockers);
+            robot.Store(new Bag());
+            var ticket = robot.Store(new Bag());
+
+            Assert.IsNull(ticket);
+        }
+
+        [Test]
+        public void should_store_bags_into_multiple_lockers_in_order()
+        {
+            var locker1 = new Locker(1);
+            var locker2 = new Locker(1);
+
+            var lockers = new List<Locker> {locker1, locker2};
+
+            var robot = new Robot(lockers);
+            var bag1 = new Bag();
+            var bag2 = new Bag();
+            var ticket1 = robot.Store(bag1);
+            var ticket2 = robot.Store(bag2);
+
+            Assert.AreSame(locker1.Pick(ticket1), bag1);
+            Assert.AreSame(locker2.Pick(ticket2), bag2);
+        }
+
+        [Test]
+        public void should_return_the_bag_stored_by_the_ticket()
+        {
+            var lockers = new List<Locker>();
+            var locker = new Locker(1);
+            lockers.Add(locker);
+
+            var robot = new Robot(lockers);
+            var bag = new Bag();
+            var ticket = robot.Store(bag);
+
+            Assert.AreSame(robot.Pick(ticket), bag);
         }
     }
 }
